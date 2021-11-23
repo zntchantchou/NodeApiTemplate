fcdea5cbb226f798438561523dd83c60f0fcfabe
z.ntchantchou <z.ntchantchou@technologyandstrategy.com>
2021-11-23 17:49:47 +0100

    corrects module resolution error due to type=module inside package.json

--- server.ts
+++ server.ts
@@ -6,3 +6,15 @@ dotenv.config()
 const app = express()
 
 app.listen(process.env.PORT, () => console.log(`[Server.js]: listening on ${process.env.PORT}...`))
+
+app.get('/', function(_, res) {
+  res.write('This is a normal request, it should be logged to the console too');
+  res.end();
+});
+
+app.get('/api', function(_, res) {
+  res.write('Response for /api');
+  res.end();
+});
+
+export {}
